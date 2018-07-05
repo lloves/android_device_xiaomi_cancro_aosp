53be964b5369b0223cfa09c900b6ab0a70c23f51
Владимир <vova.perm@hotmail.com>
2018-07-05 10:20:57 +0500

    cancro: Drop deprecated OpenGLRenderer props

--- cancro.mk
+++ cancro.mk
@@ -347,11 +347,5 @@ ifneq ($(QCPATH),)
 $(call inherit-product-if-exists, $(QCPATH)/prebuilt_HY11/target/product/msm8974/prebuilt.mk)
 endif
 
-# call dalvik heap config
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
-
-# call hwui memory config
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
-
 # call the proprietary setup
 $(call inherit-product, vendor/xiaomi/cancro/cancro-vendor.mk)
